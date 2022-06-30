0ce9de636ed35348390368f2eb08368e29f9e796
Trog19 <tsrogers8@msn.com>
2022-06-30 16:03:01 -0600

    created map function in related videos need to add more elements img, description etc. ran out of api calls for the day

--- frontend/src/pages/RelatedVideos/RelatedVideos.jsx
+++ frontend/src/pages/RelatedVideos/RelatedVideos.jsx
@@ -1,14 +1,16 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+import VideoPlayer from "../../components/VideoPlayer";
 
 
-const RelatedVideosPage = (props) => {
+
+const RelatedVideos = ({videoId}) => {
     const[relatedVideos, setRelatedVideos]= useState([])
     useEffect(() => {
  
-    const fetchVideos =async()=>{
+    const fetchRelatedVideos =async(videoId)=>{
       try { 
-        let response = await axios.get{`https://www.googleapis.com/youtube/v3/search?q=${props.videoId}&key=AIzaSyCYpL8Ms12BQUCMlecVei-ZYjgb2Kx3Ov0`}
+        let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${videoId}&key=AIzaSyCYpL8Ms12BQUCMlecVei-ZYjgb2Kx3Ov0&part=snippet`)
         setRelatedVideos(response.data);
         console.log(response.data) 
       }
@@ -16,19 +18,23 @@ const RelatedVideosPage = (props) => {
         console.log(error.response.data);
       }
     }
-fetchVideos();
+fetchRelatedVideos(videoId);
   }, []);
   
   
   
   
   return (
-    <div> 
-        <h1>HAkdmnsdnfnsd</h1>
-        <RelatedVideosPage />
+    <div>
+      <h1>Related Videos</h1>
+      {relatedVideos.map((element,index) =>{
+        if (element.snippet){
+          return <p>{element.snippet.title}</p>
+        }
+      })}
     </div>
   
   );
 };
 
-export default RelatedVideosPage;
+export default RelatedVideos      ;
--- frontend/src/pages/SearchPage/SearchPage.jsx
+++ frontend/src/pages/SearchPage/SearchPage.jsx
@@ -2,14 +2,15 @@ import React, {useEffect, useState} from "react"
 import axios from "axios";
 import SearchBar from "../../components/SearchBar";
 import VideoPlayer from "../../components/VideoPlayer";
+import RelatedVideos from "../RelatedVideos/RelatedVideos";
 
 const SearchPage = (props) => { 
     const [videoResults, setVideoResults]= useState([]);
     const[videoId, setVideoId]= useState([]);
 
-    const fetchVideos =async(videoSearch)=>{
+    const fetchVideos =async(videoSearch="programing")=>{
         try { 
-          let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${videoSearch}&key=AIzaSyCYpL8Ms12BQUCMlecVei-ZYjgb2Kx3Ov0`) 
+          let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${videoSearch}&key=AIzaSyCYpL8Ms12BQUCMlecVei-ZYjgb2Kx3Ov0&part=snippet`) 
           setVideoResults(response.data);
           setVideoId(response.data.items[0].id.videoId)
           console.log(response.data) 
@@ -22,6 +23,7 @@ const SearchPage = (props) => {
         <div>
         <SearchBar fetchVideos={fetchVideos}/>
         <VideoPlayer videoId={videoId}/> 
+        <RelatedVideos videoId={videoId}/>
        </div>
 
      );
